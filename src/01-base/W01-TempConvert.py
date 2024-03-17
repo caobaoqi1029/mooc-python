f75c84e90b82e1d675d2f47e3d36fd1064ca9580
cbq-windows <2024cbq@gmail.com>
2024-03-17 12:45:29 +0800

    style

--- src/01-base/W01-TempConvert.py
+++ src/01-base/W01-TempConvert.py
@@ -5,10 +5,10 @@ def temp_convert():
     TempStr = input("请输入带有符号的温度值: ")
     if TempStr[-1] in ['F', 'f']:
         C = (eval(TempStr[0:-1]) - 32) / 1.8
-        print("转换后的温度是{:.2f}C".format(C))
+        print(f"转换后的温度是{C:.2f}C")
     elif TempStr[-1] in ['C', 'c']:
         F = 1.8 * eval(TempStr[0:-1]) + 32
-        print("转换后的温度是{:.2f}F".format(F))
+        print(f"转换后的温度是{F:.2f}F")
     else:
         print("输入格式错误")
 
